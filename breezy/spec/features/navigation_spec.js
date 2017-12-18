d8dd3da048ea6dcb3796965368bd1083c7392f4a
johny <jho406@gmail.com>
2017-12-17 19:43:59 -0500

    fix tests

--- breezy/spec/features/navigation_spec.js
+++ breezy/spec/features/navigation_spec.js
@@ -3,6 +3,8 @@ import {start, stop} from '../../lib/index'
 import fetchMock from 'fetch-mock'
 import * as rsp from '../fixtures'
 import { render } from 'react-dom'
+import thunk from 'redux-thunk'
+import {combineReducers, createStore, applyMiddleware} from 'redux'
 import { Provider, connect } from 'react-redux'
 import React from 'react'
 import {Nav, mapStateToProps, mapDispatchToProps} from '../../lib/utils/react'
@@ -11,8 +13,8 @@ import { createMemoryHistory } from 'history'
 
 process.on('unhandledRejection', r => console.log(r));
 
-const createScene = (html) => {
-  const dom = new JSDOM(`${html}`, {runScripts: 'dangerously'})
+const createScene = (html, url='http://localhost') => {
+  const dom = new JSDOM(`${html}`, {runScripts: 'dangerously', url})
   return {dom, target: dom.window.document.body.firstElementChild}
 }
 
@@ -51,11 +53,10 @@ describe('navigation', () => {
   describe('when successfully visiting', () => {
     it('saves the page', (done) => {
       let history = createMemoryHistory({});
-      let {dom, target} = createScene(`<div></div>`)
+      let {dom, target} = createScene(`<div></div>`, 'http://localhost/bar')
 
-      const App = start({
+      const bz = start({
         window: dom.window,
-        url: '/bar',
         initialPage: {
           data: {
             heading: 'this is page 1'
@@ -64,6 +65,15 @@ describe('navigation', () => {
         },
         history
       })
+      const {reducer, initialState, Nav} = bz
+
+      const store = createStore(
+        reducer,
+        initialState,
+        applyMiddleware(thunk)
+      )
+
+      bz.connect(store)
 
       const VisibleHome = connect(
         mapStateToProps,
@@ -86,9 +96,9 @@ describe('navigation', () => {
 
 
       render(
-        <App
-          mapping={{'home': VisibleHome, 'about': VisibleAbout}}
-        />,
+        <Provider store={store}>
+          <Nav mapping={{'home': VisibleHome, 'about': VisibleAbout}}/>
+        </Provider>,
         target
       )
       fetchMock.mock('/foo', rsp.visitSuccess)
@@ -126,20 +136,27 @@ describe('navigation', () => {
   describe('when successfully grafting', () => {
     it('grafts the node', (done) => {
       let history = createMemoryHistory({});
-      let {dom, target} = createScene(`<div></div>`)
+      let {dom, target} = createScene(`<div></div>`, 'http://localhost/foo')
 
-      const App = start({
+      const bz = start({
         window: dom.window,
-        url: '/foo',
         initialPage: {
           data: {
-            heading: 'this is page 1',
-            address: {}
+            heading: 'this is page 1'
           },
           screen: 'home'
         },
         history
       })
+      const {reducer, initialState, Nav} = bz
+
+      const store = createStore(
+        reducer,
+        initialState,
+        applyMiddleware(thunk)
+      )
+
+      bz.connect(store)
 
       class ExampleHome extends Home {
         visit() {
@@ -161,10 +178,10 @@ describe('navigation', () => {
 
 
       render(
-        <App 
-          mapping={{'home': VisibleHome}}
-        />,
-      target
+        <Provider store={store}>
+          <Nav mapping={{'home': VisibleHome}}/>
+        </Provider>,
+        target
       )
 
       fetchMock.mock('/foo?_bz=address', rsp.graftSuccessWithNewZip)

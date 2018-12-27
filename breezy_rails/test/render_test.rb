39f6f55e2a426392f3cb97b8939b5f9a079f79d8
johny <jho406@gmail.com>
2018-12-27 09:35:41 -0500

    Fix render tests

--- breezy_rails/test/render_test.rb
+++ breezy_rails/test/render_test.rb
@@ -108,7 +108,7 @@ class RenderTest < ActionController::TestCase
     rendered = <<~HTML
       <html>
         <head>
-          <script>(function(){var joints={};var cache={};var defers=[];return ({"data":#{content.to_json},"screen":"#{opts[:screen]}","csrfToken":"secret","assets":["/app.js"],"joints":joints,"defers":defers});})();</script>
+          <script>(function(){var joints={};var lastJointName;var lastJointPath;var cache={};var defers=[];return ({"data":#{content.to_json},"screen":"#{opts[:screen]}","csrfToken":"secret","assets":["/app.js"],"joints":joints,"lastJointName":lastJointName,"lastJointPath":lastJointPath,"defers":defers});})();</script>
         </head>
         <body></body>
       </html>
@@ -120,7 +120,7 @@ class RenderTest < ActionController::TestCase
 
   def assert_breezy_js(content)
     assert_response 200
-    assert_equal '(function(){var joints={};var cache={};var defers=[];return ({"data":' + content.to_json + ',"screen":"render/action","csrfToken":"secret","assets":["/app.js"],"joints":joints,"defers":defers});})()', @response.body
+    assert_equal '(function(){var joints={};var lastJointName;var lastJointPath;var cache={};var defers=[];return ({"data":' + content.to_json + ',"screen":"render/action","csrfToken":"secret","assets":["/app.js"],"joints":joints,"lastJointName":lastJointName,"lastJointPath":lastJointPath,"defers":defers});})()', @response.body
     assert_equal 'text/javascript', @response.content_type
   end
 

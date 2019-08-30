a933fd9aa5efdbba4532d372d2ac004409a3708e
johny <jho406@gmail.com>
2019-08-30 15:45:48 -0400

    In Rail 6, webpack takes over for JS, so do not track
    Adding application.js to sprockets also causes an error.

--- breezy_rails/lib/install/templates/web/initializer.rb
+++ breezy_rails/lib/install/templates/web/initializer.rb
@@ -1,6 +1,15 @@
 require 'breezy_template/core_ext'
 
 Breezy.configure do |config|
-  config.track_sprockets_assets = ['application.js', 'application.css']
+  # Configure breezy.js to refresh the browser when sprockets or
+  # webpacker asset fingerpint changes. This is similar to Turbolink's
+  # `data-turbolinks-track`.
+  #
+  # Note that this file was generated without sprockets JS tracking.
+  # If you need to change this behavior, add it like so:
+  #
+  # config.track_sprockets_assets = ['application.js', 'application.css']
+  config.track_sprockets_assets = ['application.css']
+
   config.track_pack_assets = ['application.js']
 end

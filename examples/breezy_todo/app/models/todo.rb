024b18a294fabb6c2ba0f096dff3f2d35988d860
Johny Ho <johny@thoughtbot.com>
2020-02-07 12:06:26 -0500

    Remove example folder

--- examples/breezy_todo/app/models/todo.rb
+++
@@ -1,2 +0,0 @@
-class Todo < ApplicationRecord
-end
--- examples/breezy_todo/config/initializers/breezy.rb
+++
@@ -1,4 +0,0 @@
-  Breezy.configure do |config|
-    config.track_sprockets_assets = ['application.js', 'application.css']
-    config.track_pack_assets = ['application.js']
-  end

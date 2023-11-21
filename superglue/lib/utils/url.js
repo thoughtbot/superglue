1c56376636055325e18bcf0ad965732acc5ac0d7
jho406 <johny@thoughtbot.com>
2023-11-21 13:55:51 -0500

    Refactor busters (#28)
    * Remove anticache
    
    This functionality isn't needed as we can handle cache control headers on the
    server side.
    
    * Replace __=0 with format=json
    
    The original intent of __=0 was to bust the browser cache that happens
    when visiting a page in HTML format where subsequent visits to the same
    page with a different content negotiation would return the wrong format.
    
    I did not know of Rail's own `format=json` that you can add the param to help
    prevent that issue from ever happening. This replaces that.

--- superglue/lib/utils/url.js
+++ superglue/lib/utils/url.js
@@ -1,7 +1,5 @@
 import parse from 'url-parse'
 
-const uniqueId = () => Math.random().toString(36).substring(2, 10)
-
 export function pathQuery(url) {
   const { pathname, query } = new parse(url, {})
 
@@ -21,43 +19,19 @@ export function hasPropsAt(url) {
   return !!query['props_at']
 }
 
-export function withAntiCache(url) {
+export function withFormatJson(url) {
   url = new parse(url, {}, true)
+  url.query['format'] = 'json'
 
-  if (Object.prototype.hasOwnProperty.call(url.query, '_')) {
-    return url.toString()
-  } else {
-    url.query['_'] = uniqueId()
-    return url.toString()
-  }
-}
-
-export function withMimeBust(url) {
-  url = new parse(url, {}, true)
-  if (Object.prototype.hasOwnProperty.call(url.query, '__')) {
-    return url.toString()
-  } else {
-    url.query['__'] = '0'
-    return url.toString()
-  }
-}
-
-export function withoutBusters(url) {
-  url = new parse(url, {}, true)
-  let query = url.query
-  delete query['__']
-  delete query['_']
-  url.query = query
-  return pathQuery(url.toString())
+  return url.toString()
 }
 
 export function pathWithoutBZParams(url) {
   url = new parse(url, {}, true)
   let query = url.query
 
-  delete query['__']
-  delete query['_']
   delete query['props_at']
+  delete query['format']
   url.query = query
 
   return pathQueryHash(url.toString())
@@ -77,9 +51,8 @@ export function urlToPageKey(url) {
   url = new parse(url, {}, true)
   let query = url.query
 
-  delete query['__']
-  delete query['_']
   delete query['props_at']
+  delete query['format']
   url.query = query
 
   return pathQuery(url.toString())
@@ -91,12 +64,17 @@ export function withoutHash(url) {
   return url.toString()
 }
 
-export function formatForXHR(url, opts = {}) {
-  let formats = [withMimeBust, withoutHash]
+export function withoutBusters(url) {
+  url = new parse(url, {}, true)
+  let query = url.query
+  delete query['format']
+  url.query = query
 
-  if (opts.cacheRequest) {
-    formats.push(withAntiCache)
-  }
+  return pathQuery(url.toString())
+}
+
+export function formatForXHR(url) {
+  let formats = [withoutHash, withFormatJson]
 
   return formats.reduce((memo, f) => f(memo), url)
 }
--- superglue/spec/lib/utils/url.spec.js
+++ superglue/spec/lib/utils/url.spec.js
@@ -1,6 +1,4 @@
 import {
-  withAntiCache,
-  withMimeBust,
   withoutHash,
   removePropsAt,
   pathQuery,
@@ -8,26 +6,6 @@ import {
   hasPropsAt,
 } from '../../../lib/utils/url'
 import parse from 'url-parse'
-
-describe('.withAntiCache', () => {
-  it('take a url and adds an cache busting param', () => {
-    const url = withAntiCache('http://www.github.com')
-    const url2 = withAntiCache('http://www.github.com')
-    const urlHost = parse(url).host
-    const urlHost2 = parse(url).host
-
-    expect(url).not.toEqual(url2)
-    expect(urlHost).toEqual(urlHost2)
-  })
-})
-
-describe('.withMimeBust', () => {
-  it('take a url and adds mime busting param, browsers will cache same urls even if mime type is different', () => {
-    const url = withMimeBust('http://www.github.com')
-
-    expect(url).toEqual('http://www.github.com/?__=0')
-  })
-})
 
 describe('.withoutHash', () => {
   it('take a url and removes the hash', () => {

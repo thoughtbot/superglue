39b8e44717ea387b16d383a519734dce5d59c8d1
Johny Ho <johny@thoughtbot.com>
2023-12-09 10:10:03 -0500

    Remove unused Provider

--- superglue_rails/lib/install/templates/web/application.js
+++ superglue_rails/lib/install/templates/web/application.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { ApplicationBase } from '@thoughtbot/superglue';
 import { buildVisitAndRemote } from './application_visit';

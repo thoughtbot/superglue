aca7c2591620de6b3c1287a8cf53bacd1e770286
Johny Ho <johny@thoughtbot.com>
2024-06-28 15:34:41 -0400

    Add types to middleware

--- superglue/lib/middleware.ts
+++ superglue/lib/middleware.ts
@@ -1,71 +1,75 @@
 import * as actions from './actions'
+import { Dispatch, RootState } from './types'
 import { getIn } from './utils/immutability'
+import { Middleware } from 'redux'
 
-const actionValues = Object.values(actions)
+const actionValues = Object.values(actions).map((action) => action.toString())
 
-const fragmentMiddleware = (store) => (next) => (action) => {
-  const prevState = store.getState()
-  const nextAction = next(action)
-  const nextState = store.getState()
+const fragmentMiddleware: Middleware<unknown, RootState, Dispatch> =
+  (store) => (next) => (action: { payload: unknown; type: string }) => {
+    const prevState = store.getState()
+    const nextAction = next(action)
+    const nextState = store.getState()
+    const type = action.type
 
-  if (actionValues.includes(action.type)) {
-    return nextAction
-  }
+    if (actionValues.includes(type)) {
+      return nextAction
+    }
 
-  if (prevState.pages === nextState.pages) {
-    return nextAction
-  }
+    if (prevState.pages === nextState.pages) {
+      return nextAction
+    }
 
-  const changedFragments = {}
-  const changedKeys = Object.keys(nextState.pages).filter((key) => {
-    return prevState.pages[key] !== nextState.pages[key]
-  })
-
-  if (changedKeys.length === 0) {
-    return nextAction
-  }
-
-  changedKeys.forEach((key) => {
-    nextState.pages[key].fragments.forEach((fragment) => {
-      const { type, path } = fragment
-      const nextPage = nextState.pages[key]
-      const prevPage = prevState.pages[key]
-      let nextFragment, prevFragment
-
-      try {
-        prevFragment = getIn(prevPage, path)
-        nextFragment = getIn(nextPage, path)
-      } catch (err) {
-        if (err.name == 'KeyPathError') {
-          console.warn(err.message)
-        } else {
-          throw err
-        }
-      }
-
-      if (
-        nextFragment !== undefined &&
-        prevFragment !== undefined &&
-        nextFragment !== prevFragment &&
-        nextFragment
-      ) {
-        changedFragments[type] = nextFragment
-      }
+    const changedFragments = {}
+    const changedKeys = Object.keys(nextState.pages).filter((key) => {
+      return prevState.pages[key] !== nextState.pages[key]
     })
-  })
 
-  if (Object.keys(changedFragments).length === 0) {
+    if (changedKeys.length === 0) {
+      return nextAction
+    }
+
+    changedKeys.forEach((key) => {
+      nextState.pages[key].fragments.forEach((fragment) => {
+        const { type, path } = fragment
+        const nextPage = nextState.pages[key]
+        const prevPage = prevState.pages[key]
+        let nextFragment, prevFragment
+
+        try {
+          prevFragment = getIn(prevPage, path)
+          nextFragment = getIn(nextPage, path)
+        } catch (err) {
+          if (err.name == 'KeyPathError') {
+            console.warn(err.message)
+          } else {
+            throw err
+          }
+        }
+
+        if (
+          nextFragment !== undefined &&
+          prevFragment !== undefined &&
+          nextFragment !== prevFragment &&
+          nextFragment
+        ) {
+          changedFragments[type] = nextFragment
+        }
+      })
+    })
+
+    if (Object.keys(changedFragments).length === 0) {
+      return nextAction
+    }
+
+    store.dispatch({
+      type: actions.UPDATE_FRAGMENTS,
+      payload: {
+        changedFragments,
+      },
+    })
+
     return nextAction
   }
-
-  store.dispatch({
-    type: actions.UPDATE_FRAGMENTS,
-    payload: {
-      changedFragments,
-    },
-  })
-
-  return nextAction
-}
 
 export { fragmentMiddleware }

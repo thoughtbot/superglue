6611fed6870080c0bd39e8bb9f583a84b567a200
Johny Ho <johny@thoughtbot.com>
2024-07-09 21:26:58 -0400

    Make JSDOM explicit

--- superglue/spec/helpers/polyfill.js
+++ superglue/spec/helpers/polyfill.js
@@ -1,3 +1,6 @@
 import { AbortController } from 'abortcontroller-polyfill/dist/cjs-ponyfill'
+import { TextEncoder, TextDecoder } from 'util';
 
 global.AbortController = AbortController
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;

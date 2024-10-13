b54cde2162695a2721a6730b30c1a2c929f9b731
Johny Ho <johny@thoughtbot.com>
2024-10-13 17:43:38 -0400

    Convert Nav and ApplicationBase to functional components
    This PR is fairly long, but it converts the two components into its functional
    counterpart.
    
    With the `Nav` we converted to `Navigation`that exports a `NavigationProvider`
    component and a `NavigationContext`, the latter is handy if we wanted to
    access visit, remote, pageKey, and navigateTo to help users build a custom
    `<Link>` component or if they want to use the functions without passing it
    from parent to child.
    
    With `Application`, we removed `ApplicationBase`. In doing so, we've simplified
    the component to the point that if someone wanted fine grain control of building
    the redux `Provider` or `Navigation`, they can just refer to the source code and
    build their own using the same helper methods in `lib/index.ts`.
    
    We also updated the docs.

--- superglue/lib/actions.ts
+++ superglue/lib/actions.ts
@@ -47,23 +47,88 @@ export const updateFragments = createAction<{
   changedFragments: Record<string, JSONValue>
 }>('@@superglue/UPDATE_FRAGMENTS')
 
+/**
+ * A redux action you can dispatch to copy a page from one pageKey to another. Its
+ * a very useful way to create optimistic updates with a URL change. For example:
+ *
+ * ```
+ * import { copyPage, remote } from '@thoughtbot/superglue'
+ *
+ * dispatch(copyPage({ from: originalKey, to: targetKey}))
+ *
+ * ... make edits to target page and finally
+ *
+ * navigateTo(targetKey)
+ * ```
+ */
 export const copyPage = createAction<{ from: PageKey; to: PageKey }>(
   '@@superglue/COPY_PAGE'
 )
 
+/**
+ * A redux action you can dispatch to remove a page from your store.
+ *
+ * ```
+ * import { removePage } from '@thoughtbot/superglue'
+ *
+ * dispatch(removePage({ pageKey: '/delete_me_please"}))
+ * ```
+ */
 export const removePage = createAction<{ pageKey: PageKey }>(
   '@@superglue/REMOVE_PAGE'
 )
 
+/**
+ * A redux action called before a `fetch` takes place. It will fire in `remote`
+ * and `visit`. You can hook into this event in your redux slices like this:
+ *
+ * ```
+ * import { beforeFetch } from '@thoughtbot/superglue'
+ *
+ * export const exampleSlice = createSlice({
+ *  name: 'Example',
+ *  initialState: {},
+ *  extraReducers: (builder) => {
+ *    builder.addCase(beforeFetch, (state, action) => {
+ * ```
+ */
 export const beforeFetch = createAction<{ fetchArgs: FetchArgs }>(
   '@@superglue/BEFORE_FETCH'
 )
 
+/**
+ * A redux action called before a `visit` takes place. You can hook into this event
+ * in your redux slices like this:
+ *
+ * ```
+ * import { beforeVisit } from '@thoughtbot/superglue'
+ *
+ * export const exampleSlice = createSlice({
+ *  name: 'Example',
+ *  initialState: {},
+ *  extraReducers: (builder) => {
+ *    builder.addCase(beforeVisit, (state, action) => {
+ * ```
+ */
 export const beforeVisit = createAction<{
   currentPageKey: PageKey
   fetchArgs: FetchArgs
 }>('@@superglue/BEFORE_VISIT')
 
+/**
+ * A redux action called before `remote` takes place. You can hook into this event
+ * in your redux slices like this:
+ *
+ * ```
+ * import { beforeRemote } from '@thoughtbot/superglue'
+ *
+ * export const exampleSlice = createSlice({
+ *  name: 'Example',
+ *  initialState: {},
+ *  extraReducers: (builder) => {
+ *    builder.addCase(beforeRemote, (state, action) => {
+ * ```
+ */
 export const beforeRemote = createAction<{
   currentPageKey: PageKey
   fetchArgs: FetchArgs
--- superglue/lib/index.tsx
+++ superglue/lib/index.tsx
@@ -1,50 +1,47 @@
-import React from 'react'
+import React, { useEffect, forwardRef, useImperativeHandle } from 'react'
 import parse from 'url-parse'
 import { rootReducer } from './reducers'
 import { config } from './config'
 import { urlToPageKey, ujsHandlers, argsForHistory } from './utils'
 import { saveAndProcessPage } from './action_creators'
 import { historyChange, setCSRFToken } from './actions'
-import { ConnectedComponent, Provider, connect } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 
-import {
-  BrowserHistory,
-  History,
-  createBrowserHistory,
-  createMemoryHistory,
-} from 'history'
+import { History, createBrowserHistory, createMemoryHistory } from 'history'
 
-import Nav from './components/Nav'
+import { NavigationProvider } from './components/Navigation'
+export { NavigationProvider, NavigationContext } from './components/Navigation'
+export { saveAndProcessPage } from './action_creators'
 
 export {
   beforeFetch,
   beforeVisit,
   beforeRemote,
-  saveResponse,
   updateFragments,
   copyPage,
   removePage,
+  saveResponse,
   GRAFTING_ERROR,
   GRAFTING_SUCCESS,
 } from './actions'
 
 import { mapStateToProps, mapDispatchToProps } from './utils/react'
 import {
-  Remote,
   SuperglueStore,
-  Handlers,
-  Visit,
   VisitResponse,
-  PageOwnProps,
-  AllPages,
   Page,
-  JSONValue,
+  buildVisitAndRemote,
+  buildStore,
+  ConnectedMapping,
+  ApplicationProps,
 } from './types'
-// export { superglueReducer, pageReducer, rootReducer } from './reducers'
+export { superglueReducer, pageReducer, rootReducer } from './reducers'
 export { fragmentMiddleware } from './middleware'
 export { getIn } from './utils/immutability'
 export { urlToPageKey }
 export { usePage, useSuperglue } from './hooks'
+
+const hasWindow = typeof window !== 'undefined'
 
 function pageToInitialState(key: string, page: VisitResponse) {
   const slices = page.slices || {}
@@ -60,7 +57,7 @@ function pageToInitialState(key: string, page: VisitResponse) {
   }
 }
 
-function start({
+function populateStore({
   initialPage,
   baseUrl = config.baseUrl,
   maxPages = config.maxPages,
@@ -95,199 +92,139 @@ function start({
     initialPageKey,
   }
 }
-/**
- * Props for the `ApplicationBase` component
- */
-interface ApplicationProps {
-  /**
-   * The global var SUPERGLUE_INITIAL_PAGE_STATE is set by your erb
-   * template, e.g., index.html.erb
-   */
-  initialPage: VisitResponse
-  /**
-   * The base url prefixed to all calls made by `visit` and
-   * `remote`.
-   */
-  baseUrl: string
-  /**
-   * The path of the current page. It should equal to the `location.pathname` +
-   * `location.search` + `location.hash`
-   */
-  path: string
-  /**
-   * The app element that was passed to React's `createRoot`. This will be used
-   * to setup UJS helpers.
-   */
-  appEl: HTMLElement
-  /**
-   * A store to perform setup on. If none is provided, it will build a basic
-   * store.
-   */
-  store?: SuperglueStore
+
+const createHistory = () => {
+  if (hasWindow) {
+    // This is used for client side rendering
+    return createBrowserHistory({})
+  } else {
+    // This is used for server side rendering
+    return createMemoryHistory({})
+  }
 }
 
-type ConnectedMapping = Record<
-  string,
-  ConnectedComponent<React.ComponentType, PageOwnProps>
->
-/**
- * The entry point to your superglue application. You should create a class
- * (Application) that inherit from the ApplicationBase component and override
- * the {@link buildStore}, {@link mapping}, and {@link visitAndRemote} methods.
- *
- * This would be setup for you when installing Superglue at `application.js`.
- */
-export abstract class ApplicationBase extends React.Component<ApplicationProps> {
-  private hasWindow: boolean
-  private navigatorRef: React.RefObject<Nav>
-  private initialPageKey: string
-  private store: SuperglueStore
-  private history: History
-  private connectedMapping: ConnectedMapping
-  private ujsHandlers: Handlers
-  private visit: Visit
-  private remote: Remote
-
-  /**
-   * The constructor of the `ApplicationBase` class.
-   * @param props
-   */
-  constructor(props: ApplicationProps) {
-    super(props)
-    this.hasWindow = typeof window !== 'undefined'
-
-    // Create a navigator Ref for UJS attributes and to enhance the base `visit`
-    // and `visit` thunks
-    this.navigatorRef = React.createRef()
-
-    // Retrieve initial values and methods to prepare the store.
-    const { prepareStore, initialState, initialPageKey, reducer } = start({
-      initialPage: this.props.initialPage,
-      baseUrl: this.props.baseUrl,
-      path: this.props.path,
+export function start(
+  initialPage: VisitResponse,
+  baseUrl: string,
+  path: string,
+  buildStore: buildStore,
+  mapping: Record<string, React.ComponentType>,
+  buildVisitAndRemote: buildVisitAndRemote,
+  navigatorRef: React.RefObject<typeof NavigationProvider>,
+  history: History
+) {
+  const { prepareStore, initialState, initialPageKey, reducer } = populateStore(
+    {
+      initialPage,
+      baseUrl,
+      path,
       // The max number of pages to keep in the store. Default is 20
       // maxPages: 20
-    })
-    this.initialPageKey = initialPageKey
-
-    // Build the store and pass Superglue's provided reducer to be combined with
-    // your reducers located at `application_reducer.js`
-    this.store = this.buildStore(initialState, reducer)
-
-    // Fire initial events and populate the store
-    prepareStore(this.store)
-
-    // Build history
-    this.history = this.createHistory()
-    this.history.replace(...argsForHistory(this.props.path))
-
-    const unconnectedMapping = this.mapping()
-    const nextMapping: ConnectedMapping = {}
-    for (const key in unconnectedMapping) {
-      const component = unconnectedMapping[key]
-      nextMapping[key] = connect(mapStateToProps, mapDispatchToProps)(component)
     }
+  )
 
-    this.connectedMapping = nextMapping
+  const store = buildStore(initialState, reducer)
+  // Fire initial events and populate the store
+  prepareStore(store)
 
-    // Build visit and remote thunks
-    // Your modified `visit` and `remote` will get passed below to the
-    // Nav component then to your components
-    //
-    // You can access them via `this.props.visit` or `this.props.remote`. In
-    // your page components
-    const { visit, remote } = this.visitAndRemote(this.navigatorRef, this.store)
-    this.visit = visit
-    this.remote = remote
+  // const history = createHistory()
+  history.replace(...argsForHistory(path))
+
+  const unconnectedMapping = mapping
+  const nextMapping: ConnectedMapping = {}
+  for (const key in unconnectedMapping) {
+    const component = unconnectedMapping[key]
+    nextMapping[key] = connect(mapStateToProps, mapDispatchToProps)(component)
   }
 
-  /**
-   * Override this method to return a visit and remote function. These functions
-   * will be used by Superglue to power its UJS attributes and passed to your
-   * page components. You may customize this functionality to your liking, e.g,
-   * adding a progress bar.
-   *
-   * @param navigatorRef
-   * @param store
-   *
-   * @returns
-   */
-  abstract visitAndRemote(
-    // eslint-disable-next-line
-    navigatorRef: React.RefObject<Nav>,
-    store: SuperglueStore
-  ): { visit: Visit; remote: Remote }
+  const connectedMapping = nextMapping
+  const { visit, remote } = buildVisitAndRemote(navigatorRef, store)
 
-  componentDidMount(): void {
-    const { appEl } = this.props
-    // Create the ujs event handlers. You can change the ujsAttributePrefix
-    // in the event the data attribute conflicts with another.
-    this.ujsHandlers = ujsHandlers({
-      visit: this.visit,
-      remote: this.remote,
-      ujsAttributePrefix: 'data-sg',
-    })
-    const { onClick, onSubmit } = this.ujsHandlers
-
-    appEl.addEventListener('click', onClick)
-    appEl.addEventListener('submit', onSubmit)
-  }
-
-  componentWillUnmount(): void {
-    const { appEl } = this.props
-    const { onClick, onSubmit } = this.ujsHandlers
-
-    appEl.removeEventListener('click', onClick)
-    appEl.removeEventListener('submit', onSubmit)
-  }
-
-  /**
-   * Override this method and return a Redux store for Superglue to use. This
-   * would be setup and generated for you in `store.js`. We recommend using
-   * using Redux toolkit's `configureStore` to build the store.
-   *
-   * @param initialState - A preconfigured intial state to pass to your store.
-   * @param reducer - A preconfigured reducer
-   */
-  abstract buildStore(
-    initialState: { pages: AllPages; [key: string]: JSONValue },
-    reducer: typeof rootReducer
-  ): SuperglueStore
-
-  createHistory(): BrowserHistory {
-    if (this.hasWindow) {
-      // This is used for client side rendering
-      return createBrowserHistory({})
-    } else {
-      // This is used for server side rendering
-      return createMemoryHistory({})
-    }
-  }
-
-  /**
-   * Override this method and return a mapping between a componentIdentifier and
-   * a PageComponent. This will be passed to Superglue to determine which Page component
-   * to render with which payload.
-   *
-   * @returns
-   */
-  abstract mapping(): Record<string, React.ComponentType>
-
-  render(): JSX.Element {
-    // The Nav component is pretty bare and can be inherited from for custom
-    // behavior or replaced with your own.
-    return (
-      <Provider store={this.store}>
-        <Nav
-          store={this.store}
-          ref={this.navigatorRef}
-          visit={this.visit}
-          remote={this.remote}
-          mapping={this.connectedMapping}
-          history={this.history}
-          initialPageKey={this.initialPageKey}
-        />
-      </Provider>
-    )
+  return {
+    store,
+    visit,
+    remote,
+    connectedMapping,
+    history,
+    initialPageKey,
   }
 }
+
+/**
+ * The entry point to your superglue application. It sets up the redux Provider,
+ * redux state and the Navigation component.
+ *
+ * This is a simple component, you can override this by copying the source code and
+ * use the exported methods used by this component (`start` and `ujsHandler`).
+ */
+const Application = forwardRef(function Application(
+  {
+    initialPage,
+    baseUrl,
+    path,
+    buildStore,
+    buildVisitAndRemote,
+    history,
+    mapping,
+    appEl,
+  }: ApplicationProps,
+  ref
+) {
+  const navigatorRef = React.createRef<typeof NavigationProvider>()
+
+  history = history || createHistory()
+
+  const { store, visit, remote, connectedMapping, initialPageKey } = start(
+    initialPage,
+    baseUrl,
+    path,
+    buildStore,
+    mapping,
+    buildVisitAndRemote,
+    navigatorRef,
+    history
+  )
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        store,
+      }
+    },
+    []
+  )
+  const handlers = ujsHandlers({
+    visit,
+    remote,
+    ujsAttributePrefix: 'data-sg',
+  })
+
+  useEffect(() => {
+    const { onClick, onSubmit } = handlers
+    appEl.addEventListener('click', onClick)
+    appEl.addEventListener('submit', onSubmit)
+
+    return () => {
+      appEl.removeEventListener('click', onClick)
+      appEl.removeEventListener('submit', onSubmit)
+    }
+  })
+
+  // The Nav component is pretty bare and can be inherited from for custom
+  // behavior or replaced with your own.
+  return (
+    <Provider store={store}>
+      <NavigationProvider
+        ref={navigatorRef}
+        visit={visit}
+        remote={remote}
+        mapping={connectedMapping}
+        history={history}
+        initialPageKey={initialPageKey}
+      />
+    </Provider>
+  )
+})
+
+export { Application }

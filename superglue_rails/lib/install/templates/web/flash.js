dfee4316fcc90be6b19bc0b94a8e05e3aafd5318
Johny Ho <johny@thoughtbot.com>
2024-10-16 07:46:46 -0400

    Update flash.js
    This gives a slice some new functionality based on the needs of
    existing projects.

--- superglue_rails/lib/install/templates/web/flash.js
+++ superglue_rails/lib/install/templates/web/flash.js
@@ -1,19 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 import { saveResponse, beforeVisit } from '@thoughtbot/superglue'
 
 export const flashSlice = createSlice({
   name: 'flash',
   initialState: {},
+  reducers: {
+    clearFlash() {
+      return {};
+    },
+    flash(state, { payload }) {
+      return payload;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(beforeVisit, (state, action) => {
-      return {}
-    })
+      return {};
+    });
     builder.addCase(saveResponse, (state, action) => {
       const { page } = action.payload;
 
       return {
-        ...state, ...page.slices.flash
-      }
-    })
-  }
-})
+        ...state,
+        ...page.slices.flash,
+      };
+    });
+  },
+});
+
+export const { clearFlash, flash } = flashSlice.actions;
+
+

33d3c14c303be226359e9f0b98dd30cc96830d1e
Johny Ho <johny@thoughtbot.com>
2024-12-09 21:47:03 -0500

    wip

--- superglue_rails/lib/generators/superglue/install/install_generator.rb
+++ superglue_rails/lib/generators/superglue/install/install_generator.rb
@@ -13,7 +13,62 @@ module Superglue
         desc: "Use typescript"
 
       def create_files
-        @use_typescript = options["typescript"]
+        use_typescript = options["typescript"]
+        if use_typescript
+          copy_js_files
+        else
+          copy_ts_files
+        end
+
+        say "Copying Superglue initializer"
+        copy_file "#{__dir__}/templates/initializer.rb", "config/initializers/superglue.rb"
+
+        say "Copying application.json.props"
+        copy_file "#{__dir__}/templates/application.json.props", "app/views/layouts/application.json.props"
+
+        say "Adding required member methods to ApplicationRecord"
+        add_member_methods
+
+        say "Installing FormProps"
+        run "bundle add form_props"
+
+        say "Installing Superglue and friends"
+        run "yarn add history react react-dom @reduxjs/toolkit react-redux @thoughtbot/superglue --save"
+
+        say "Superglue is Installed! 🎉", :green
+      end
+
+      private
+
+      def copy_ts_files
+        say "Copying application.tsx file to #{app_js_path}"
+        copy_file "#{__dir__}/templates/ts/application.tsx", "#{app_js_path}/application.tsx"
+
+        say "Copying page_to_page_mapping.js file to #{app_js_path}"
+        copy_file "#{__dir__}/templates/js/page_to_page_mapping.js", "#{app_js_path}/page_to_page_mapping.js"
+
+        say "Copying flash.ts file to #{app_js_path}"
+        copy_file "#{__dir__}/templates/ts/flash.ts", "#{app_js_path}/slices/flash.ts"
+
+        say "Copying pages.js file to #{app_js_path}"
+        copy_file "#{__dir__}/templates/js/pages.js", "#{app_js_path}/slices/pages.js"
+
+        say "Copying store.js file to #{app_js_path}"
+        copy_file "#{__dir__}/templates/js/store.js", "#{app_js_path}/store.js"
+
+        say "Copying application_visit.js file to #{app_js_path}"
+        copy_file "#{__dir__}/templates/js/application_visit.ts", "#{app_js_path}/application_visit.ts"
+
+        say "Copying components to #{app_js_path}"
+        copy_file "#{__dir__}/templates/js/inputs.js", "#{app_js_path}/components/Inputs.js"
+        copy_file "#{__dir__}/templates/js/layout.js", "#{app_js_path}/components/Layout.js"
+        copy_file "#{__dir__}/templates/js/components.js", "#{app_js_path}/components/index.js"
+
+        say "Copying jsconfig.json file to #{app_js_path}"
+        copy_file "#{__dir__}/templates/js/jsconfig.json", "jsconfig.json"
+      end
+
+      def copy_js_files
         say "Copying application.js file to #{app_js_path}"
         copy_file "#{__dir__}/templates/js/application.js", "#{app_js_path}/application.js"
 
@@ -39,26 +94,7 @@ module Superglue
 
         say "Copying jsconfig.json file to #{app_js_path}"
         copy_file "#{__dir__}/templates/js/jsconfig.json", "jsconfig.json"
-
-        say "Copying Superglue initializer"
-        copy_file "#{__dir__}/templates/initializer.rb", "config/initializers/superglue.rb"
-
-        say "Copying application.json.props"
-        copy_file "#{__dir__}/templates/application.json.props", "app/views/layouts/application.json.props"
-
-        say "Adding required member methods to ApplicationRecord"
-        add_member_methods
-
-        say "Installing FormProps"
-        run "bundle add form_props"
-
-        say "Installing Superglue and friends"
-        run "yarn add history react react-dom @reduxjs/toolkit react-redux @thoughtbot/superglue --save"
-
-        say "Superglue is Installed! 🎉", :green
       end
-
-      private
 
       def add_member_methods
         inject_into_file "app/models/application_record.rb", after: "class ApplicationRecord < ActiveRecord::Base\n" do

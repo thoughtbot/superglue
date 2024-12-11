cf59adcc5bb980338ecab8fdee4553d36ba92f70
Johny Ho <johny@thoughtbot.com>
2024-12-11 05:33:49 -0500

    done

--- superglue_rails/lib/generators/superglue/view_collection/templates/ts/edit.tsx
+++ superglue_rails/lib/generators/superglue/view_collection/templates/ts/edit.tsx
@@ -44,7 +44,7 @@ export default function <%= js_plural_table_name(:upper) %>Edit() {
         <%- attributes.each do |attr| -%>
         <<%= js_component(attr)%> {...inputs.<%= attr.column_name.camelize(:lower)%>} label="<%= attr.column_name.humanize %>" errorKey="<%= attr.column_name %>" />
         <%- end -%>
-        <SubmitButton {...inputs.submit} type="submit"> {inputs.submit.text} </button>
+        <SubmitButton {...inputs.submit} type="submit"> {inputs.submit.text} </SubmitButton>
       </Form>
 
       <a href={<%= js_singular_table_name %>Path} data-sg-visit>Show</a>

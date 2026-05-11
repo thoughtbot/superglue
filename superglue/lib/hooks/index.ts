8b608747338b034b63c36537d8ccf104c212f5de
Johny Ho <johny@thoughtbot.com>
2026-05-11 15:47:44 -0400

    Add useSetFlash

--- superglue/lib/hooks/index.ts
+++ superglue/lib/hooks/index.ts
@@ -1,5 +1,10 @@
-import { useSelector } from 'react-redux'
+import { useCallback } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { RootState, SuperglueState, FlashState } from '../types'
+import {
+  flash as flashAction,
+  clearFlash as clearFlashAction,
+} from '../actions'
 export { useContent, unproxy } from './useContent'
 export { useFragment, toFragmentRef } from './useFragment'
 export { useUpdateFragment } from './useUpdateFragment'
@@ -25,4 +30,36 @@ export function useSuperglue() {
  */
 export function useFlash<T = FlashState>(): T {
   return useSelector<RootState, T>((state) => state.flash as T)
+}
+
+/**
+ * A hook that returns functions to set and clear flash messages
+ * on the client side.
+ *
+ * ```ts
+ * const { setFlash, clearFlash } = useSetFlash()
+ *
+ * setFlash({ notice: 'Saved!' })
+ * clearFlash('notice')
+ * clearFlash() // clears all
+ * ```
+ */
+export function useSetFlash() {
+  const dispatch = useDispatch()
+
+  const setFlash = useCallback(
+    (flash: FlashState) => {
+      dispatch(flashAction({ flash }))
+    },
+    [dispatch]
+  )
+
+  const clearFlash = useCallback(
+    (key?: string) => {
+      dispatch(clearFlashAction({ key }))
+    },
+    [dispatch]
+  )
+
+  return { setFlash, clearFlash }
 }

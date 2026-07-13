350e9773021de9760216266da717fa0a7b626c9f
Johny Ho <johny@thoughtbot.com>
2026-07-12 22:08:36 -0400

    hooks: extract useSuperglue to its own file

--- superglue/lib/hooks/index.ts
+++ superglue/lib/hooks/index.ts
@@ -1,5 +1,3 @@
-import { useSelector } from 'react-redux'
-import { RootState, SuperglueState } from '../types'
 export { useContent, unproxy } from './useContent'
 export { useFragment, toFragmentRef } from './useFragment'
 export { useUpdateFragment } from './useUpdateFragment'
@@ -7,10 +5,4 @@ export { useUpdateContent } from './useUpdateContent'
 export { useStreamSource } from './useStreamSource'
 export { useFlash } from './useFlash'
 export { useSetFlash } from './useSetFlash'
-
-/**
- * A lightweight hook that grabs the superglue state from the store.
- */
-export function useSuperglue() {
-  return useSelector<RootState, SuperglueState>((state) => state.superglue)
-}
+export { useSuperglue } from './useSuperglue'
